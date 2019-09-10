78a019c09154927acae6b0ec7a1084eff8d38a70
Alcapsulator <54436611+Alcapsulator@users.noreply.github.com>
2019-09-10 12:08:02 +0200

    Add files via upload

---
+++ ListAllInstalledAppsInTeam_GraphAPI.ps1
@@ -0,0 +1,11 @@
+﻿function List-TeamApps($bearertoken, $groupid) {
+    $api = @"
+    https://graph.microsoft.com/v1.0/teams/GROUPID/installedApps?`$expand=teamsAppDefinition
+"@
+    $apicall = $api.Replace("GROUPID",$groupid)
+    $apps = Invoke-WebRequest -Method GET -Uri $apicall -Headers @{Authorization="Bearer $bearertoken"} -UseBasicParsing -ContentType "application/json"
+    $inner = $apps.Content | ConvertFrom-Json
+    ($inner.Value).teamsAppDefinition
+}
+
+List-TeamApps $btoken 37aae96d-8de5-4eb4-824b-f0627ace6702
